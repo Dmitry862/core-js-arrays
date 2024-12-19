3118b37ac5a4d3e89da4c661512c034e8767f722
Dmitry862 <rk3tt@mail.ru>
2024-12-19 21:59:59 +0300

    feat: add new solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -364,8 +364,10 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  // throw new Error('Not implemented');
+  const result = new Array(len).fill(0);
+  return result.map((_, i) => 2 * i + 1);
 }
 
 /**
